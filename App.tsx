382a011570dc400a962d8d810a324af19764d8bf
Francis Lagares <francis.lagares@gmail.com>
2022-04-11 21:07:42 +0200

    style: add background gradient

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+});
